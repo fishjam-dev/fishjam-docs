0a4d6c23b81afdc67f483cfb71fb1a6bdc53cbc2
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2023-03-09 10:50:39 +0100

    Initial docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,10 +39,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/jellyfish-dev/jellyfish-docs',
+          routeBasePath: '/',
         },
         blog: false,
         theme: {
